a4b370a8d03c6fbd351790c9648999857d2da7e2
DotEmms <emmadanckaert@hotmail.com>
2021-01-25 10:36:13 +0100

    Math test klasse aangevuld

---
+++ TestNinja.UnitTest/MathTests.cs
@@ -0,0 +1,90 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTest
+{
+    public class MathTests
+    {
+        private Math _math;
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Math();
+        }
+
+        /*Test methods usually follow this convention
+        * MethodName_Scenario_ExpectedResult
+        * GivenTheArguments_WhenMethodCalled_ThenExpectedResults
+        */
+        [Test]
+        public void Add_WhenCalled_ReturnsSumOfArguments()
+        {
+            //ASSIGN
+            int numA = 5;
+            int numB = 7;
+            //ACT / When
+            int result = _math.Add(numA,numB);
+
+            //ASSERT / Then
+            Assert.AreEqual(12,result);
+            //exact hetzelfde, geen van beide is beter
+            //Assert.That(result, Is.EqualTo(12)); 
+
+        }
+        [TestCase(2,1,2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(4, 4, 4)]
+        public void Max_WhenCalled_ReturnBiggerNumber(int numA, int numB, int expectedResult)
+        {
+            //ACT / When
+            int result = _math.Max(numA, numB);
+
+            //ASSERT / Then
+            Assert.AreEqual(expectedResult, result);
+
+        }
+        
+        /*[Test]
+        *public void Max_SecondArgumentIsGreater_SecondArgumentIsReturned()
+        *{
+        *    //ARRANGE / Given
+        *    int numA = 1;
+        *    int numB = 2;
+        *
+        *    //ACT / When
+        *    int result = _math.Max(numA, numB);
+        *
+        *    //ASSERT / Then
+        *    Assert.AreEqual(2, result);
+        *
+        *}
+        *[Test]
+        *public void Max_ArgumentsAreEqual_SameValueIsReturned()
+        *{
+        *    //ARRANGE / Given
+        *    int numA = 2;
+        *    int numB = 2;
+        *
+        *    //ACT / When
+        *    int result = _math.Max(numA, numB);
+        *
+        *    //ASSERT / Then
+        *    Assert.AreEqual(2, result);
+        *}
+        */
+
+        [Test]
+        public void GetOddNumbers_WhenGivenLimitHigherThanZero_ReturnOddNumber()
+        {
+            int[] expectedResult = { 1, 3, 5 };
+            IEnumerable<int> result = _math.GetOddNumbers(5);
+            
+            //Assert.NotNull(result);
+            //Assert.AreEqual(3, result.Count());
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+    
+}
--- TestNinja/Fundamentals/Math.cs
+++ TestNinja/Fundamentals/Math.cs
@@ -5,10 +5,11 @@ namespace TestNinja.Fundamentals
     public class Math
     {
         public int Add(int a, int b)
-        { 
-            return a + b;
+        {
+            var result = a + b;
+            return result;
         }
-        
+
         public int Max(int a, int b)
         {
             return (a > b) ? a : b;
@@ -18,7 +19,7 @@ namespace TestNinja.Fundamentals
         {
             for (var i = 0; i <= limit; i++)
                 if (i % 2 != 0)
-                    yield return i; 
+                    yield return i; //yield = keyword to build a dynamical list
         }
     }
 }
